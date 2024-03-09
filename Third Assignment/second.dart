9fe28a2c55ff037bac54b612f528c1d42a863ca2
danialahmed11 <162842631+danialahmed11@users.noreply.github.com>
2024-03-10 03:22:06 +0500

    Add files via upload

---
+++ Third Assignment/second.dart
@@ -0,0 +1,13 @@
+void main (){
+
+int age = 20;
+if (age < 20){
+
+  print("Your are Youngest");
+}
+else{
+
+  print("Your are oldest");
+}
+
+}
---
+++ Third Assignment/third.dart
@@ -0,0 +1,17 @@
+void main (){
+int number = 6;
+if(number < 0){
+print("The numnber is negative");
+
+}
+else if(number > 0){
+
+  print("The number is positive");
+}
+
+else if(number == 0){
+
+  print("The number is equal to 0");
+}
+
+}
